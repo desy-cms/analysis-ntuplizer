1a09e5b34b4336a52c2785de25acc58f460d1028
Roberval Walsh <roberval.walsh@gmail.com>
2021-06-18 11:43:13 +0200

    assume version passed via yaml, remove path version and duplicates path,l1,objs on-the-fly

--- python/TriggerInfo_cfi.py
+++ python/TriggerInfo_cfi.py
@@ -21,25 +21,41 @@ def triggerInfo(info_file):
       with open(info_file) as f:
          info_data = yaml.safe_load(f)
       for path, path_info in sorted(info_data.items()):
-         p = str(path)
-         triggerPaths.append(p)
-         triggerConditions.append(p+'*')
+         p = str(path).strip()
+         p = "_".join(p.split("_")[:-1])+"_v" # remove version
+         # remove path duplicates
+         if not p in triggerPaths:
+            triggerPaths.append(p)
+            triggerConditions.append(p+'*')
          l1s = info_data[path]['l1seeds']
          for l1 in sorted(l1s):
             if l1 == '':
                continue
-            l1Seeds.append(str(l1))
+            l1str = str(l1).strip()
+            #remove l1 seed duplicates
+            if not l1str in l1Seeds:  
+               l1Seeds.append(l1str)
          objs = info_data[path]['trigger_objects']
-         for obj in objs:
+         for obj in sorted(objs):
             if obj == '':
                continue
-            triggerObjects.append(str(obj))
-            if 'hltL1' in obj and 'Mu' in obj and 'Jet' in obj:
-               trgObjL1MuJet.append(str(obj))
-               trgObjL1MuJetType.append('l1muon:l1jet')
+            objstr = str(obj).strip()
+            # remove trigger object duplicates
+            if not objstr in triggerObjects:
+               triggerObjects.append(objstr)
+               if 'hltL1' in objstr and 'Mu' in objstr and 'Jet' in objstr:
+                  trgObjL1MuJet.append(objstr)
+                  trgObjL1MuJetType.append('l1muon:l1jet')
    else:
       print '>>>>>>>> Msg-W: The given YAML file with trigger info does not exist <<<<<<<<'
       print ''
+      
+   # sort lists
+   triggerPaths.sort()
+   triggerConditions.sort()
+   l1Seeds.sort()  
+   triggerObjects.sort()
+   
    triggerResultsFilter = cms.PSet(triggerConditions = triggerConditions)
    ntuplizerTriggerPaths = cms.PSet(TriggerPaths = triggerPaths)
    ntuplizerL1Seeds = cms.PSet(L1Seeds = l1Seeds)
