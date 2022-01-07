6aaf5ffc2414aa9a56982a058d08d4e274644ea8
Roberval Walsh <roberval.walsh@gmail.com>
2022-01-07 14:43:18 +0100

    adding opts.version to cmsRun pyCfgParams

--- python/ntp_utils/ntp_crab.py
+++ python/ntp_utils/ntp_crab.py
@@ -76,7 +76,7 @@ class ntp_crab:
       config.JobType.maxMemoryMB = 10000
       config.JobType.inputFiles = [self.__versiondir+'/trigger_info.yml']
       # Passing cmsRun parameters
-      config.JobType.pyCfgParams = ["year="+str(self.__opts.year),"type="+self.__opts.type,"triggerInfo="+self.__versiondir+"/trigger_info.yml"]
+      config.JobType.pyCfgParams = ["year="+str(self.__opts.year),"type="+self.__opts.type,"triggerInfo="+self.__versiondir+"/trigger_info.yml","version="+self.__opts.version]
       if info:
          for var,value in info.iteritems():
             if var=='xsection_pb':
